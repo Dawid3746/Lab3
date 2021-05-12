d9df4bfa00d3f60b217699aa74b1728342ac22d7
Dawid Sałek <dawidsalekst@gmail.com>
2021-05-13 01:04:18 +0200

    Dodanie TextField

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -2,13 +2,19 @@ package sample;
 
 import javafx.event.ActionEvent;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 
 public class Controller {
     public Button btnMain;
+    public TextField txtLicznik;
     int clickCounter = 0;
 
 
     public void OnAction(ActionEvent actionEvent) {
-        System.out.println("Klawisz kliknięty po raz "+ ++clickCounter);
+        System.out.println("Klawisz kliknięty po raz " + ++clickCounter);
+        txtLicznik.setText("Klawisz kliknięty po raz " + ++clickCounter);
+        if (clickCounter == 1) {
+            btnMain.setText("I jeszcze raz");
+        }
     }
 }
