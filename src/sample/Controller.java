4278bd717e4bb6c7c27b9d66f5ecdb492e65e80f
Dawid Sałek <dawidsalekst@gmail.com>
2021-05-13 01:32:48 +0200

    Kalkulator kosztu stworzenia walca stalowego

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -3,18 +3,41 @@ package sample;
 import javafx.event.ActionEvent;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Spinner;
+import javafx.scene.control.SpinnerValueFactory;
+import java.util.ResourceBundle;
+import java.net.URL;
 
-public class Controller {
-    public Button btnMain;
-    public TextField txtLicznik;
-    int clickCounter = 0;
+public class Controller implements Initializable {
 
 
-    public void OnAction(ActionEvent actionEvent) {
-        System.out.println("Klawisz kliknięty po raz " + ++clickCounter);
-        txtLicznik.setText("Klawisz kliknięty po raz " + ++clickCounter);
-        if (clickCounter == 1) {
-            btnMain.setText("I jeszcze raz");
+            public Spinner<Double> spinnerWysokosc;
+            public SpinnerValueFactory<Double> svfWysokosc = new SpinnerValueFactory.DoubleSpinnerValueFactory(1.0, 250.0, 1.0);
+
+            public Spinner<Double> spinnerPromien;
+            public SpinnerValueFactory<Double> svfPromien = new SpinnerValueFactory.DoubleSpinnerValueFactory(1.0, 50.0, 1.0, 0.5);
+
+            public Spinner<Double> spinnerGrubosc;
+            public SpinnerValueFactory<Double> svfGrubosc = new SpinnerValueFactory.DoubleSpinnerValueFactory(0.0, 50.0, 0.5, 0.1);
+
+            public TextField txtKoszt;
+
+            public TextField txtObjetosc;
+
+            @Override
+            public void initialize(URL url, ResourceBundle resourceBundle){
+                spinnerWysokosc.setValueFactory(svfWysokosc);
+                spinnerGrubosc.setValueFactory(svfGrubosc);
+                spinnerPromien.setValueFactory(svfPromien);
+            }
+
+            public void onBtnAction(ActionEvent actionEvent) {
+                double promien = spinnerWysokosc.getValue() * Math.PI * (Math.pow(spinnerPromien.getValue(), 2) - Math.pow(spinnerGrubosc.getValue(), 2));
+                txtObjetosc.setText(Double.toString(Math.round(promien * 100.0) / 100.0));
+
+                double waga = promien * 0.001;
+                txtKoszt.setText((Double.toString(Math.round(waga * 2.70 * 100.0) / 100.0) + "/" + Double.toString(Math.round(waga * 14.0 * 100.0) / 100.0)));
+            }
+
         }
-    }
-}
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -11,8 +11,8 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setTitle("Kalkulator kosztu stworzenia walca stalowego");
+        primaryStage.setScene(new Scene(root, 384, 405));
         primaryStage.show();
     }
 
