25354429dd2a848936c5755a244df153a9db25d5
Dawid Sałek <dawidsalekst@gmail.com>
2021-05-13 00:55:10 +0200

    Stworzenie przycisku z licznikiem kliknięć

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,14 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+
 public class Controller {
+    public Button btnMain;
+    int clickCounter = 0;
+
+
+    public void OnAction(ActionEvent actionEvent) {
+        System.out.println("Klawisz kliknięty po raz "+ ++clickCounter);
+    }
 }
